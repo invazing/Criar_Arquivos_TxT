ac8d8ee643e19f70ccb8ff6342b8c93b4b0f6c59
Invazing <invazing@hotmail.com>
2021-06-18 14:15:21 -0300

    Adicionar arquivos de projeto.

---
+++ WindowsApplication2/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim fileExists As Boolean
+        fileExists = My.Computer.FileSystem.FileExists("C:\Users\Public\Desktop\TranspenTI.txt")
+        If fileExists = False Then
+            If MessageBox.Show("O Relatorio não esta criado deseja criar ?", "logfile", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = Windows.Forms.DialogResult.Yes Then
+                My.Computer.FileSystem.WriteAllText("C:\Users\Public\Desktop\TranspenTI.txt", String.Empty, False)
+            End If
+        End If
+    End Sub
+    Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+    End Sub
+End Class
